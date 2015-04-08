1eb5b98ddeba788de681fe07ba9c814ae0be7254
Sajjad Tabib <sajjad@imojiapp.com>
2015-04-07 18:05:50 -0700

    more updates for the sdksample

--- sdksample/src/main/java/com/imojiapp/imoji/sdksample/MainActivity.java
+++ sdksample/src/main/java/com/imojiapp/imoji/sdksample/MainActivity.java
@@ -1,39 +1,93 @@
 package com.imojiapp.imoji.sdksample;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import com.imojiapp.imoji.sdk.Callback;
+import com.imojiapp.imoji.sdk.Imoji;
+import com.imojiapp.imoji.sdk.ImojiApi;
+
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 
 public class MainActivity extends ActionBarActivity {
+private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    @InjectView(R.id.gv_imoji_grid)
+    GridView mImojiGrid;
+    private ImojiApi mImojiApi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+
+        ImojiApi.Builder builder = new ImojiApi.Builder("wasup dawg");
+        mImojiApi = builder.build();
+        mImojiApi.getFeatured(new Callback<List<Imoji>>() {
+            @Override
+            public void onSuccess(List<Imoji> result) {
+                ImojiAdapter adapter = new ImojiAdapter(MainActivity.this, R.layout.imoji_item_layout, result);
+                mImojiGrid.setAdapter(adapter);
+            }
+
+            @Override
+            public void onFailure() {
+                Log.d(LOG_TAG, "failure");
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private class ImojiAdapter extends ArrayAdapter<Imoji> {
+    
+        private LayoutInflater mInflater;
+    
+        public ImojiAdapter(Context context, int resource, List<Imoji> items) {
+            super(context, resource, items);
+            mInflater = LayoutInflater.from(context);
         }
+    
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+            if(convertView == null){
+                convertView = mInflater.inflate(R.layout.imoji_item_layout, parent, false);
+                holder = new ViewHolder(convertView);
+                convertView.setTag(holder);
+            }else{
+                holder = (ViewHolder)convertView.getTag();
+            }
+            
+            Imoji item = getItem(position);
+            mImojiApi.loadThumb(getContext(), item, null).into(holder.mImojiIv);
+            
+    
+            return convertView;
+        }
+    }
+    
+    static class ViewHolder{
 
-        return super.onOptionsItemSelected(item);
+        @InjectView(R.id.iv_imoji)
+        ImageView mImojiIv;
+
+        public ViewHolder(View v){
+            ButterKnife.inject(this, v);
+        }
     }
 
 }
