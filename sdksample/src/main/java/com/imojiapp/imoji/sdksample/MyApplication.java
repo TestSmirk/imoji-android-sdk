04220c8fd98f32fecb88c32edde28c19dcc99a61
Sajjad Tabib <sajjad@imojiapp.com>
2015-05-11 16:44:32 -0700

    keep inner class so that the builder doesn't get stripped out remove launcher icons from library

--- sdksample/src/main/java/com/imojiapp/imoji/sdksample/MyApplication.java
+++ sdksample/src/main/java/com/imojiapp/imoji/sdksample/MyApplication.java
@@ -3,6 +3,7 @@ package com.imojiapp.imoji.sdksample;
 import android.app.Application;
 
 import com.imojiapp.imoji.sdk.ImojiApi;
+import com.imojiapp.imoji.sdk.ImojiApi.Builder;
 
 /**
  * Created by sajjadtabib on 4/17/15.
@@ -12,6 +13,7 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        ImojiApi.init(this, "93c89ce0-d3ee-4697-bfd8-2e0e5fc72bb6", "U2FsdGVkX1+hI9aV1dXW1qY0gcjsbTrE53bPGbHTJ6JwByWThceSki5RiGTpy1e/IdBe/vU3qpZUTPZ34XPcAQ==");
+        ImojiApi api = new Builder(this).build();
+        ImojiApi.init(this, "93c89ce0-d3ee-4697-bfd8-2e0e5fc72bb6", "U2FsdGVkX1+hI9aV1dXW1qY0gcjsbTrE53bPGbHTJ6JwByWThceSki5RiGTpy1e/IdBe/vU3qpZUTPZ34XPcAQ==", api);
     }
 }
