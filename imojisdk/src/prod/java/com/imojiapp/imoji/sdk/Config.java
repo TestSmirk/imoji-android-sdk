15fc2b7f648724dd4fc3bdc0c920da0711d93099
Sajjad Tabib <sajjad@imojiapp.com>
2015-05-19 15:12:59 -0700

    hide config info

--- imojisdk/src/prod/java/com/imojiapp/imoji/sdk/Config.java
+++ imojisdk/src/prod/java/com/imojiapp/imoji/sdk/Config.java
@@ -4,6 +4,6 @@ package com.imojiapp.imoji.sdk;
  * Created by sajjadtabib on 4/6/15.
  */
 public class Config {
-    public static final String BASE_URL = "https://api.imoji.io/v2";
+    static final String BASE_URL = "https://api.imoji.io/v2";
     static final String IMOJI_APP_PACKAGE = "com.imojiapp.imoji.prod";
 }
