fec4a7a3c935a16e5c055338a2f52726a033f5d3
Sajjad Tabib <sajjad@imojiapp.com>
2015-05-19 15:12:59 -0700

    hide config info

--- imojisdk/src/local/java/com/imojiapp/imoji/sdk/Config.java
+++ imojisdk/src/local/java/com/imojiapp/imoji/sdk/Config.java
@@ -3,7 +3,7 @@ package com.imojiapp.imoji.sdk;
 /**
  * Created by sajjadtabib on 4/6/15.
  */
-public class Config {
-    public static final String BASE_URL = "http://10.0.1.6:8081/v2";
+class Config {
+    static final String BASE_URL = "http://10.0.1.6:8081/v2";
     static final String IMOJI_APP_PACKAGE = "com.imojiapp.imoji.local";
 }
