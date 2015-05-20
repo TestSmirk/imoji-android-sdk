a9bd531f051de97bf1820a794fd2a37b2a9ce1a3
Sajjad Tabib <sajjad@imojiapp.com>
2015-05-19 17:16:56 -0700

    refactor api execution. add dependency execution

--- imojisdk/src/main/java/com/imojiapp/imoji/sdk/ExternalGrantReceiver.java
+++ imojisdk/src/main/java/com/imojiapp/imoji/sdk/ExternalGrantReceiver.java
@@ -3,7 +3,6 @@ package com.imojiapp.imoji.sdk;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
 /**
  * Created by sajjad on 5/2/15.
@@ -20,7 +19,7 @@ public abstract class ExternalGrantReceiver extends BroadcastReceiver {
 
         if (intent.hasExtra(ExternalIntents.BundleKeys.EXTERNAL_OAUTH_TOKEN_BUNDLE_ARG_KEY)) {
             String receivedToken = intent.getStringExtra(ExternalIntents.BundleKeys.EXTERNAL_OAUTH_TOKEN_BUNDLE_ARG_KEY);
-            String savedToken = SharedPreferenceManager.getString(ImojiApi.PrefKeys.EXTERNAL_TOKEN, null);
+            String savedToken = SharedPreferenceManager.getString(PrefKeys.EXTERNAL_TOKEN, null);
 
             if (receivedToken != null && savedToken != null && receivedToken.equals(savedToken)) {
                 mForUs = true;
---
+++ imojisdk/src/main/java/com/imojiapp/imoji/sdk/PrefKeys.java
@@ -0,0 +1,13 @@
+package com.imojiapp.imoji.sdk;
+
+/**
+ * Created by sajjadtabib on 5/19/15.
+ */
+interface PrefKeys {
+    String CLIENT_ID_PROPERTY = "c";
+    String CLIENT_SECRET_PROPERTY = "s";
+    String TOKEN_PROPERTY = "t";
+    String EXPIRATION_PROPERTY = "e";
+    String REFRESH_PROPERTY = "r";
+    String EXTERNAL_TOKEN = "external_token";
+}
