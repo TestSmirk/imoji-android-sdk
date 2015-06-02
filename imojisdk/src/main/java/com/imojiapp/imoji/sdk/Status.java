4a1cb71ab72fb83028b370e57ab2421c2ccd0751
Sajjad Tabib <sajjad@imojiapp.com>
2015-06-02 08:49:42 -0700

    wrap start activity methods in an ActivityNotFoundException

--- imojisdk/src/main/java/com/imojiapp/imoji/sdk/Status.java
+++ imojisdk/src/main/java/com/imojiapp/imoji/sdk/Status.java
@@ -8,6 +8,7 @@ public class Status {
     public static final String FAILED = "FAILED";
     public static final String NETWORK_ERROR = "NETWORK_ERROR";
     public static final String LAUNCH_PLAYSTORE = "LAUNCH_PLAYSTORE";
+    public static final String PLAYSTORE_NOT_FOUND = "PLAYSTORE_NOT_FOUND";
     public static final String IMOJI_UPDATE_REQUIRED = "IMOJI_UPDATE_REQUIRED";
     public static final String TIMEOUT_FAILURE = "TIMEOUT_FAILURE";
 
