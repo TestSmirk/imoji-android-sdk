beb79ebd38feb766522e7b7f0cf2a09a8f51c41d
Sajjad Tabib <sajjad@imojiapp.com>
2015-08-03 17:49:29 -0700

    Grant user access and get user imojis sample

--- sdksample/src/main/java/com/imojiapp/imoji/sdksample/MainActivity.java
+++ sdksample/src/main/java/com/imojiapp/imoji/sdksample/MainActivity.java
@@ -1,39 +1,20 @@
 package com.imojiapp.imoji.sdksample;
 
-import android.app.Activity;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.GridView;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import com.imojiapp.imoji.sdk.Callback;
-import com.imojiapp.imoji.sdk.Imoji;
-import com.imojiapp.imoji.sdk.ImojiApi;
-import com.imojiapp.imoji.sdk.ImojiCategory;
-import com.imojiapp.imoji.sdksample.adapters.ImojiAdapter;
-import com.squareup.picasso.Picasso;
-
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import butterknife.ButterKnife;
@@ -43,19 +24,8 @@ import butterknife.InjectView;
 public class MainActivity extends ActionBarActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
-
-
     @InjectView(R.id.lv_samples)
     ListView mSamples;
-
-//    @InjectView(R.id.gv_imoji_grid)
-//    GridView mImojiGrid;
-//
-//    @InjectView(R.id.et_search)
-//    EditText mSearchEt;
-//
-//    @InjectView(R.id.iv_full_imoji)
-//    ImageView mFullImoji;
 
     private SampleAdapter mAdapter;
 
@@ -67,7 +37,7 @@ public class MainActivity extends ActionBarActivity {
         ButterKnife.inject(this);
 
         try {
-            ActivityInfo[] activityInfos = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_ACTIVITIES|PackageManager.GET_META_DATA).activities;
+            ActivityInfo[] activityInfos = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_ACTIVITIES | PackageManager.GET_META_DATA).activities;
 
             List<ActivityInfo> sampleActivities = new ArrayList<>();
             for (ActivityInfo info : activityInfos) {
@@ -92,137 +62,6 @@ public class MainActivity extends ActionBarActivity {
             e.printStackTrace();
         }
 
-
-//        Picasso myPicasso = new Picasso.Builder(this).build();
-//        ImojiApi.with(this).setPicassoInstance(myPicasso);
-//
-//        mImojiGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                Imoji imoji = (Imoji) parent.getItemAtPosition(position);
-//                ImojiApi.with(MainActivity.this).loadFull(imoji).into(mFullImoji);
-//            }
-//        });
-//
-//        mImojiGrid.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-//            @Override
-//            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-//                Imoji imoji = (Imoji) parent.getItemAtPosition(position);
-//
-//                ImojiApi.with(MainActivity.this).addImojiToUserCollection(imoji.getImojiId(), new Callback<String, String>() {
-//                    @Override
-//                    public void onSuccess(String result) {
-//                        Log.d(LOG_TAG, "Add Imoji Success with result: " + result);
-//                    }
-//
-//                    @Override
-//                    public void onFailure(String result) {
-//                        Log.d(LOG_TAG, "Add Imoji Failed with result: " + result);
-//                    }
-//                });
-//
-//
-//                return false;
-//            }
-//
-//        });
-//
-//        ImojiApi.with(this).getFeatured(new Callback<List<Imoji>, String>() {
-//            @Override
-//            public void onSuccess(List<Imoji> result) {
-//                Log.d(LOG_TAG, "imoji: " + result.get(0).toString());
-//
-//
-//                ImojiAdapter adapter = new ImojiAdapter(MainActivity.this, R.layout.imoji_item_layout, result);
-//                mImojiGrid.setAdapter(adapter);
-//            }
-//
-//            @Override
-//            public void onFailure(String error) {
-//                Log.d(LOG_TAG, "failure: " + error);
-//            }
-//        });
-//
-//        mSearchEt.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-//            @Override
-//            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-//                if (actionId == EditorInfo.IME_ACTION_DONE) {
-//                    String query = v.getText().toString();
-//                    ImojiApi.with(MainActivity.this).search(query, new Callback<List<Imoji>, String>() {
-//                        @Override
-//                        public void onSuccess(List<Imoji> result) {
-//                            ImojiAdapter adapter = new ImojiAdapter(MainActivity.this, R.layout.imoji_item_layout, result);
-//                            mImojiGrid.setAdapter(adapter);
-//                        }
-//
-//                        @Override
-//                        public void onFailure(String error) {
-//                            Log.d(LOG_TAG, "failed with error: " + error);
-//                        }
-//                    });
-//                    return true;
-//                }
-//                return false;
-//            }
-//        });
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        if (item.getItemId() == R.id.action_create_imoji) {
-            ImojiApi.with(this).createImoji();
-            return true;
-        }else if (item.getItemId() == R.id.action_external_oauth) {
-            ImojiApi.with(this).initiateUserOauth(new Callback<String, String>() {
-                @Override
-                public void onSuccess(String result) {
-                    Log.d(LOG_TAG, "success: " + result);
-                }
-
-                @Override
-                public void onFailure(String err) {
-                    Log.d(LOG_TAG, "error: " + err);
-                }
-            });
-            return true;
-        }else if (item.getItemId() == R.id.action_get_user_imojis) {
-            ImojiApi.with(this).getUserImojis(new Callback<List<Imoji>, String>() {
-                @Override
-                public void onSuccess(List<Imoji> result) {
-                    Log.d(LOG_TAG, "got user imojis: " + result.size());
-                }
-
-                @Override
-                public void onFailure(String error) {
-                    Log.d(LOG_TAG, "failed to get user imojis: error");
-                }
-            });
-        }else if (item.getItemId() == R.id.action_get_categories) {
-            ImojiApi.with(this).getImojiCategories(ImojiCategory.Classification.GENERIC, new Callback<List<ImojiCategory>, String>() {
-                @Override
-                public void onSuccess(List<ImojiCategory> result) {
-                    for (ImojiCategory c : result) {
-                        Log.d(LOG_TAG, "" + c.getTitle());
-                    }
-                }
-
-                @Override
-                public void onFailure(String result) {
-
-                }
-            });
-
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     private class SampleAdapter extends ArrayAdapter<ActivityInfo> {
@@ -237,12 +76,12 @@ public class MainActivity extends ActionBarActivity {
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             ViewHolder holder;
-            if(convertView == null){
+            if (convertView == null) {
                 convertView = mInflater.inflate(R.layout.sample_item, parent, false);
                 holder = new ViewHolder(convertView);
                 convertView.setTag(holder);
-            }else{
-                holder = (ViewHolder)convertView.getTag();
+            } else {
+                holder = (ViewHolder) convertView.getTag();
             }
 
             ActivityInfo item = getItem(position);
@@ -251,11 +90,11 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
-    static class ViewHolder{
+    static class ViewHolder {
         @InjectView(R.id.tv_sample_name)
         TextView mSampleName;
 
-        public ViewHolder(View v){
+        public ViewHolder(View v) {
             ButterKnife.inject(this, v);
         }
     }
--- sdksample/src/main/java/com/imojiapp/imoji/sdksample/UserImojisActivity.java
+++ sdksample/src/main/java/com/imojiapp/imoji/sdksample/UserImojisActivity.java
@@ -1,37 +1,115 @@
 package com.imojiapp.imoji.sdksample;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.TextView;
+
+import com.imojiapp.imoji.sdk.Callback;
+import com.imojiapp.imoji.sdk.Imoji;
+import com.imojiapp.imoji.sdk.ImojiApi;
+import com.imojiapp.imoji.sdksample.adapters.ImojiAdapter;
+
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class UserImojisActivity extends Activity {
+
+    private static final String LOG_TAG = UserImojisActivity.class.getSimpleName();
+
+    @InjectView(R.id.bt_get_user_imojis)
+    Button mGetUserImojis;
+
+    @InjectView(R.id.bt_get_user_access)
+    Button mGetUserAccess;
+
+    @InjectView(R.id.tv_granted)
+    TextView mGrantedTv;
+
+    @InjectView(R.id.gv_imoji_grid)
+    GridView mImojiGrid;
+
+    @InjectView(R.id.ll_container)
+    View mContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_imojis);
+        ButterKnife.inject(this);
+
+        mGetUserAccess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getUserAccess();
+            }
+        });
+
+        mGetUserImojis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getUserImojis();
+            }
+        });
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_user_imojis, menu);
-        return true;
+    private void getUserAccess() {
+        ImojiApi.with(this).initiateUserOauth(new Callback<String, String>() {
+            @Override
+            public void onSuccess(String result) {
+                Log.d(LOG_TAG, "success: " + result);
+                mGrantedTv.setText("TRUE");
+                mGrantedTv.setTextColor(Color.GREEN);
+                mContainer.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onFailure(String err) {
+                Log.d(LOG_TAG, "error: " + err);
+                mGrantedTv.setText("FALSE");
+                mGrantedTv.setTextColor(Color.RED);
+            }
+        });
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private void getUserImojis() {
+        ImojiApi.with(UserImojisActivity.this).getUserImojis(new Callback<List<Imoji>, String>() {
+            @Override
+            public void onSuccess(List<Imoji> result) {
+                ImojiAdapter adapter = new ImojiAdapter(UserImojisActivity.this, R.layout.imoji_item_layout, result);
+                mImojiGrid.setAdapter(adapter);
+            }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            @Override
+            public void onFailure(String result) {
+                Log.w(LOG_TAG, "failed: " + result);
+
+            }
+        });
+    }
+
+
+
+
+    /**
+     * Created by sajjadtabib on 5/1/15.
+     */
+    public static class ExternalGrantReceiver extends com.imojiapp.imoji.sdk.ExternalGrantReceiver {
+        private static final String LOG_TAG = ExternalGrantReceiver.class.getSimpleName();
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            super.onReceive(context, intent);
+            Log.d(LOG_TAG, "external grant receiver granted: " + mGranted);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
